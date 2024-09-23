e44bb9222d86408d56bc58752dde688d5f7f0fcc
Ehsanur Rahman Rhythm <errhythm.me@gmail.com>
2024-09-23 20:31:41 +0600

    feat: integrate SpeedInsights component in _app.tsx
    Added the SpeedInsights component from @vercel/speed-insights/next to the _app.tsx file to monitor and improve page performance.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Component {...pageProps} />
+            <SpeedInsights />
+        </>
+    )
 }
 
 export default MyApp
